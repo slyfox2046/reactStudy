1b0eddaf47c826880ce661592342325a77dddb11
slyfox2046 <slyfox2046@gmail.com>
2020-11-23 09:19:01 +0800

    server

---
+++ react-redux-login-stage2-server/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function App() { 
+  return (
+    <div className="jumbotron">
+      <h1>Hi React Redux Login</h1>
+    </div>
+  )
+}
+export default App; 
---
+++ react-redux-login-stage2-server/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import { composeWithDevTools } from "redux-devtools-extension"
+import { createStore, applyMiddleware } from "redux"
+import { Provider} from "react-redux"
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+ </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
