b7a84886639b2ed7e434c254733b2e1f8c69054d
slyfox2046 <slyfox2046@gmail.com>
2020-11-23 16:23:03 +0800

    Router

--- react-redux-login-stage3-router/src/components/NavigationBar.jsx
+++ react-redux-login-stage3-router/src/components/NavigationBar.jsx
@@ -2,24 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 class NavigationBar extends React.Component {
   render() {
-    return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
       <div className="container">
         <Link className="navbar-brand" to="/">
           Login功能
         </Link>
-        <button className="nav-toggler" type="button" data-toggle="collapse" data-target="">
+        <button
+          type="button"
+          class="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-1"
+          aria-expanded="false"
+          >
+            
+
           <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarsExample05">
-          <ul className="navbar-nav mr-auto">
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbarsExample05">
+          {/* <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/signup">注册</Link>
+              <Link className="nav-link" to="/signup">
+                注册
+              </Link>
             </li>
-    </ul>
-        </div>
+          </ul> */}
+            
+            <Link className="navbar-brand" to="/signup">
+          注册
+        </Link>
+          </div>
+          
+   
+
+
       </div>
-    </nav>)
+    </nav>
+    );
   }
 }
-export default NavigationBar
+export default NavigationBar;
